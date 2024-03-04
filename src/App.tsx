cf2a212ce2fa0991f240908232b47dd1773fdd0b
vasotodorovic <vaso@dynato.com>
2024-03-04 23:59:28 +0100

    optimizate import

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import ClientSearch from "./components/ClientSearch";
-import { Grid } from "@mui/material";
+
 import TableClient from "./components/Table";
-import ClientHead from "./components/table/ClienHead";
 function App() {
-  //
 
-  const [count, setCount] = useState(0);
-
-  var render = 0;
-  // useEffect run on every render
   return (
     <div>
       <TableClient></TableClient>
